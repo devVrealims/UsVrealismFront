004c8d4b2ba8ba144626772a1b64ccbbe1ccf928
VRealism <95053345+devVrealims@users.noreply.github.com>
2022-08-03 19:11:21 -0500

    Create Dash.js

---
+++ dashboard/src/components/dashboard/Dash.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  Form,
+  Button,
+  FloatingLabel,
+  FormGroup,
+  Label,
+  Nav,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export const Dash = () => {
+  return (
+    <div>
+      dash
+      <Form.Group>
+        <Button>Enter</Button>
+      </Form.Group>
+    </div>
+  );
+};
